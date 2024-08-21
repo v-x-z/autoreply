e21a714722a43d66649145b89f4d5ddaccb31868
v-x-z <138826670+v-x-z@users.noreply.github.com>
2024-08-21 05:51:42 +0100

    Update reply.py

--- reply.py
+++ reply.py
@@ -3,9 +3,6 @@ from discord.ext import commands
 
 bot = commands.Bot(command_prefix='!')  
 
-# Replace 'YOUR_FRIEND_ID' with the ID of your friend
-FRIEND_ID = 1123402776630792254
-
 @bot.event  
 async def on_message(message):  
     if message.author == bot.user:  
@@ -16,12 +13,10 @@ async def on_message(message):
             await bot.process_commands(message)  
         else:  
             if bot.user.mentioned_in(message):  
-                await message.channel.send('ily toz')
-            elif any(user.id == FRIEND_ID for user in message.mentions):
-                await message.channel.send('ily toz')
+                await message.channel.send('your_reply_here')
 
 @bot.event  
 async def on_ready():  
     print(f'{bot.user} has connected to Discord!')  
 
-bot.run('MTEwNjI0NTk0MjkzMzQ1ODk1NQ.G1xMFd.Qh5EpkEy1joPWeJAUXD_099lbM5bXmzFAF4V-k', bot=False)
+bot.run('TOKEN', bot=False)
