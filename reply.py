8e9d6a0126bf02da8a3e9306b8998c8b2ebcfa4b
v-x-z <138826670+v-x-z@users.noreply.github.com>
2024-08-29 15:44:18 +0100

    Update reply.py

--- reply.py
+++ reply.py
@@ -1,5 +1,6 @@
 import discord  
-from discord.ext import commands  
+from discord.ext import commands
+from config import TOKEN
 
 bot = commands.Bot(command_prefix='!')  
 
@@ -7,16 +8,12 @@ bot = commands.Bot(command_prefix='!')
 async def on_message(message):  
     if message.author == bot.user:  
         return  
-
-    if message.type == discord.MessageType.default:  
-        if message.content.startswith('!'):  
-            await bot.process_commands(message)  
-        else:  
-            if bot.user.mentioned_in(message):  
-                await message.channel.send('your_reply_here') # this is the reply you want people to see
+     
+    if bot.user.mentioned_in(message):  
+        await message.channel.send('your_reply_here') # this is the reply you want people to see
 
 @bot.event  
 async def on_ready():  
     print(f'{bot.user} your autoreplying has started!')
 
-bot.run('YOUR_TOKEN', bot=False) # your discord token goes here in 'your_token' if you dont know how to get your token search up a guide 
+bot.run(TOKEN, bot=False) # your discord token goes here in 'your_token' if you dont know how to get your token search up a guide 
